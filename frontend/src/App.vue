11e3cd6e4b6b01545a055ba97c6b6443cc92a9ff
Alejandro <alejandro.orostica@usach.cl>
2021-03-31 01:05:01 -0300

    style in app improved

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <router-view/>
+    <v-app>
+      <router-view/>
+    </v-app>
   </div>
 </template>
 
 
 <style scoped> 
 #app {
-  position: absolute !important;
   width: 100% !important;
   height: 100% !important;
   z-index: -1 !important;
-  html, body { height: 100%; width: 100%; font-family: Open Sans, serif !important;}
-  body { margin: 0; padding: 0; background: #F7F7F7;}
+  font-family: Arial, Helvetica, sans-serif;
 }
 </style>
