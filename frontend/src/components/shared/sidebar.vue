43a3e4cd8349230b70fcb09413aa36c7be6d7518
Alejandro <alejandro.orostica@usach.cl>
2021-03-31 01:05:55 -0300

    sidebar updated

--- frontend/src/components/shared/sidebar.vue
+++ frontend/src/components/shared/sidebar.vue
@@ -16,9 +16,9 @@
           <v-list-item link>
             <v-list-item-content>
               <v-list-item-title class="title">
-                Sandra Adams
+                {{name}}
               </v-list-item-title>
-              <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+              <v-list-item-subtitle>{{email}}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -29,13 +29,19 @@
           nav
           dense
         >
-          <v-list-item link>
+          <v-list-item link to="/home/products">
             <v-list-item-icon>
-              <v-icon>mdi-folder</v-icon>
+              <v-icon>mdi-format-list-bulleted-square</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Productos</v-list-item-title>
           </v-list-item>
-          <v-list-item link>
+          <v-list-item link to="/home/add">
+            <v-list-item-icon>
+              <v-icon>mdi-file-plus</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Agregar Item</v-list-item-title>
+          </v-list-item>
+          <v-list-item link to="/home">
             <v-list-item-icon>
               <v-icon>mdi-account-multiple</v-icon>
             </v-list-item-icon>
@@ -44,3 +50,15 @@
         </v-list>
       </v-navigation-drawer>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      id: localStorage.getItem('id') || 0,
+      name: localStorage.getItem('name') || '',
+      email: localStorage.getItem('email') || '',
+    }
+  }
+}
+</script>
