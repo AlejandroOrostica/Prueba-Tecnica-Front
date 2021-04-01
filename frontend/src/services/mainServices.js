4b5bb1fceaf8813f359f00fb3f6d0c0c6606f377
Alejandro <alejandro.orostica@usach.cl>
2021-04-01 00:32:38 -0300

    services added
    Added services for  edit, and delete a product
    Added services for view logs

--- frontend/src/services/mainServices.js
+++ frontend/src/services/mainServices.js
@@ -14,8 +14,25 @@ export default {
     getProducts(){
         return axios.get(API_URL + 'products/')
     },
+
+    getProduct(id) {
+        return axios.get(API_URL + `products/${id}`)
+    },
     
     addProduct(userId, data){
         return axios.post(API_URL + `products/add/${userId}`, data )
+    },
+
+    editProduct(userId, data, productId) {
+
+        return axios.put(API_URL + `products/${userId}/update/${productId}`, data)
+    },
+
+    deleteProduct(userId, productId) {
+        return axios.delete(API_URL + `products/${userId}/delete/${productId}`)
+    },
+
+    getLogs() {
+        return axios.get(API_URL + `logs`)
     }
 }
